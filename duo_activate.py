de1d3c4597a2368a3fc1cbd475ca045b18d339b7
Chris T <github@chris.talbothome.com>
2021-02-22 13:08:29 -0500

    Update duo_activate.py

--- duo_activate.py
+++ duo_activate.py
@@ -6,37 +6,31 @@ import base64
 import json
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python duo_bypass.py <url to duo qr>")
-    sys.exit()
-
-qr_url = sys.argv[1]
-
-host = 'api-%s' % (qr_url.split('/')[2].split('-')[1],)
-code = qr_url.rsplit('/',1)[1]
+host = 'api-XXXXX.duosecurity.com'
+code = 'XXXXXXXXXX'
 
 url = 'https://{host}/push/v2/activation/{code}?customer_protocol=1'.format(host=host, code=code)
 headers = {'User-Agent': 'okhttp/2.7.5'}
 data = {'jailbroken': 'false',
-        'architecture': 'armv7',
+        'architecture': 'arm64',
         'region': 'US',
         'app_id': 'com.duosecurity.duomobile',
         'full_disk_encryption': 'true',
         'passcode_status': 'true',
         'platform': 'Android',
-        'app_version': '3.23.0',
+        'app_version': '3.49.0',
         'app_build_number': '323001',
-        'version': '8.1',
+        'version': '11',
         'manufacturer': 'unknown',
         'language': 'en',
-        'model': 'Pixel C',
-        'security_patch_level': '2018-12-01'}
+        'model': 'Pixel 3a',
+        'security_patch_level': '2021-02-01'}
 
 r = requests.post(url, headers=headers, data=data)
 response = json.loads(r.text)
 
 try:
-  secret = base64.b32encode(response['response']['hotp_secret'])
+  secret = base64.b32encode(response['response']['hotp_secret'].encode())
 except KeyError:
   print(response)
   sys.exit(1)
@@ -46,14 +40,13 @@ print("secret", secret)
 print("10 Next OneTime Passwords!")
 # Generate 10 Otps!
 hotp = pyotp.HOTP(secret)
-for _ in xrange(10):
+for _ in range(10):
     print(hotp.at(_))
 
 f = open('duotoken.hotp', 'w')
-f.write(secret + "\n")
+f.write(secret.decode() + "\n")
 f.write("0")
 f.close()
 
 with open('response.json', 'w') as resp:
     resp.write(r.text)
-
